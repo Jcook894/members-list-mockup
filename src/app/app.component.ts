ddc423ce2cb4ace07c39aa93e9dd58d7ed420c54
Julian Cook <jcook@smcpartners.com>
2019-08-01 14:28:59 -0400

    Set layout/styling of members list mockup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,61 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'list-screen-mockup';
+  /**
+   * Dummy member data
+   */
+  memberList = [
+    {
+      MassHealthID: 534253425234,
+      MedicaidID: 56789,
+      RiskScore: 9000,
+      LastName: 'Cook',
+      FirstName: 'Julian',
+      DOB: '03/08/1994',
+      PCP: 'S N',
+      CNS: 'C',
+      CA: 'S C',
+      CP: 'N S',
+      CHW: 'S C'
+    },
+    {
+      MassHealthID: 9785897648764,
+      MedicaidID: 56789,
+      RiskScore: 8000,
+      LastName: 'Iannuci',
+      FirstName: 'Gino',
+      DOB: '03/08/1994',
+      PCP: 'S N',
+      CNS: 'C',
+      CA: 'S C',
+      CP: 'N S',
+      CHW: 'S C'
+    },
+    {
+      MassHealthID: 1234637567599,
+      MedicaidID: 56789,
+      RiskScore: 7000,
+      LastName: 'Dill',
+      FirstName: 'Jason',
+      DOB: '03/08/1994',
+      PCP: 'S N',
+      CNS: 'C',
+      CA: 'S C',
+      CP: 'N S',
+      CHW: 'S C'
+    },
+    {
+      MassHealthID: 6546546756876,
+      MedicaidID: 56789,
+      RiskScore: 6000,
+      LastName: 'Reider',
+      FirstName: 'Dylan',
+      DOB: '03/08/1994',
+      PCP: 'S N',
+      CNS: 'C',
+      CA: 'S C',
+      CP: 'N S',
+      CHW: 'S C'
+    },
+  ];
 }
